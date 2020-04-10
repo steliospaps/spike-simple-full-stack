e5c5ac47ceb036a4e1196402742b62832ebfa8e1
stelios <stelios05@gmail.com>
2020-04-10 21:02:15 +0100

    backend

---
+++ backend/src/main/java/io/github/steliospaps/echo/EchoController.java
@@ -0,0 +1,21 @@
+package io.github.steliospaps.echo;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@RestController
+@Slf4j
+public class EchoController {
+
+	@RequestMapping(path = "/echo" , method = RequestMethod.POST)
+	public Mono<EchoResponse> echo(@RequestBody EchoRequest request) {
+		log.info("got {}",request);
+		return Mono.just(new EchoResponse("hello "+request.getRequest()));
+	}
+}
