535c2e0aa6b6e866c8b9a998105d03e5b277c8cd
stelios <stelios05@gmail.com>
2020-04-25 23:20:09 +0100

    WIP: starts but only with public addresses

--- deploy/api-gateway-s3-eb/eb.tf
+++ deploy/api-gateway-s3-eb/eb.tf
@@ -1,28 +1,100 @@
 resource "aws_elastic_beanstalk_application" "tftest" {
-  name        = "tf-test-name"
+  name        = "tf-test-app-name"
   description = "tf-test-desc"
-/*
+
   appversion_lifecycle {
-    service_role          = "${aws_iam_role.beanstalk_service.arn}"
+    service_role          = aws_iam_role.eb_service.arn
     max_count             = 128
     delete_source_from_s3 = true
   }
-*/
+
   tags = merge(
     local.common_tags
   )
 
 }
 /*
-resource "aws_elastic_beanstalk_configuration_template" "tf_template" {
-  name                = "tf-test-template-config"
-  application         = "${aws_elastic_beanstalk_application.tftest.name}"
-  solution_stack_name = "64bit Amazon Linux 2 v3.0.0 running Corretto 11"
+resource "aws_iam_instance_profile" "eb_service" {
+    name = "beanstalk-service-profile"
+    role = aws_iam_role.eb_service.name
+}
+*/
+resource "aws_iam_role" "eb_service" {
+    name = "beanstalk-service-role"
+    assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "elasticbeanstalk.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole",
+      "Condition": {
+        "StringEquals": {
+          "sts:ExternalId": "elasticbeanstalk"
+        }
+      }
+    }
+  ]
+}
+EOF
+  tags = merge(
+    local.common_tags
+  )
+}
+
+resource "aws_iam_policy_attachment" "eb_service" {
+    name = "elastic-beanstalk-service"
+    roles = [aws_iam_role.eb_service.id]
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSElasticBeanstalkService"
+}
+resource "aws_iam_policy_attachment" "eb_service_health" {
+    name = "elastic-beanstalk-service"
+    roles = [aws_iam_role.eb_service.id]
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSElasticBeanstalkEnhancedHealth"
+}
+
+
+
+# eb_ec2 roles
+resource "aws_iam_instance_profile" "eb_ec2" {
+  name_prefix = "eb_ec2-"
+  role = aws_iam_role.eb_ec2.name
+}
+
+resource "aws_iam_role" "eb_ec2" {
+  name_prefix = "eb_ec2-"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
 
   tags = merge(
     local.common_tags
   )
-}*/
+
+}
+
+resource "aws_iam_role_policy_attachment" "eb_ec2" {
+  role = aws_iam_role.eb_ec2.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier"
+}
+
+
 
 locals {
   #namespace=>name=>value
@@ -32,12 +104,38 @@ locals {
       "MinSize" = "1"
       "MaxSize" = "1"
     }
+    "aws:autoscaling:launchconfiguration" ={
+      "IamInstanceProfile" = aws_iam_instance_profile.eb_ec2.name
+      //TODO: make this a conditional property
+      "EC2KeyName" = "laptop-key"
+      "SecurityGroups" = aws_security_group.eb_ec2.id
+    }
     "aws:ec2:instances"={
-      "InstanceTypes" = "t3.nano,t3a.nano"
+      "InstanceTypes" = "t3.nano,t3a.micro"
     }
     "aws:ec2:vpc" = {
-      "AssociatePublicIpAddress" = false
+      "AssociatePublicIpAddress" = true
+      "VPCId" = local.vpc.id
+      "Subnets" = join(",",local.subnets.*.id)
+      "ELBSubnets" = join(",",local.subnets.*.id)
     }
+    "aws:elasticbeanstalk:environment" = {
+      # LoadBalanced or SingleInstance
+      "EnvironmentType" = "LoadBalanced"
+      // classic application network
+      "LoadBalancerType" = "application"
+      "ServiceRole"= aws_iam_role.eb_service.name
+    }
+    "aws:elasticbeanstalk:healthreporting:system" = {
+      // basic enhanced
+      "SystemType" = "enhanced"
+      // Ok Warning Degraded Severe //TODO: revisit this one it works
+      "HealthCheckSuccessThreshold" = "Severe"
+    }
+    "aws:elbv2:loadbalancer" = {
+      "SecurityGroups" = aws_security_group.eb_lb.id
+    }
+
   }
 
   ebConfigFlat = flatten([
@@ -52,16 +150,18 @@ locals {
 }
 
 resource "aws_elastic_beanstalk_environment" "tfenvtest" {
-  name                = "tf-test-name"
-  application         = aws_elastic_beanstalk_application.tftest.name
-  solution_stack_name = "64bit Amazon Linux 2 v3.0.0 running Corretto 11"
-
   tags = merge(
     local.common_tags
   )
+  name                = "tf-test-env-name"
+  application         = aws_elastic_beanstalk_application.tftest.name
+  solution_stack_name = "64bit Amazon Linux 2 v3.0.0 running Corretto 11"
+
+  #default 20m
+  wait_for_ready_timeout="20m"
 
   #see https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/beanstalk-environment-configuration-advanced.html
-
+  #see https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/command-options-general.html
   dynamic "setting" {
     for_each = local.ebConfigFlat
     content {
