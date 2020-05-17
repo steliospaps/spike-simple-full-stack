01823b5c07d6b1906931f64646222011ecfe914e
stelios <stelios05@gmail.com>
2020-05-17 19:58:54 +0100

    backend: cors from env properties, so that it can be overriden in deployment

--- src/backend/src/main/java/io/github/steliospaps/echo/EchoApplication.java
+++ src/backend/src/main/java/io/github/steliospaps/echo/EchoApplication.java
@@ -1,7 +1,11 @@
 package io.github.steliospaps.echo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @SpringBootApplication
 public class EchoApplication {
@@ -10,4 +14,16 @@ public class EchoApplication {
 		SpringApplication.run(EchoApplication.class, args);
 	}
 
+	
+	@Bean
+    public WebFluxConfigurer corsConfigurer(@Value("${cors.allowed-origins}") String[] origins,
+    		@Value("${cors.allowed-methods}") String[] methods) {
+        return new WebFluxConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/*").allowedOrigins(origins).allowedMethods(methods);
+                
+            }
+        };
+    }
 }
--- src/backend/src/test/java/io/github/steliospaps/echo/EchoControllerTest.java
+++ src/backend/src/test/java/io/github/steliospaps/echo/EchoControllerTest.java
@@ -1,7 +1,5 @@
 package io.github.steliospaps.echo;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.web.reactive.function.BodyInserter;
-import org.springframework.web.reactive.function.BodyInserters;
 
 @ExtendWith(SpringExtension.class)
 //We create a `@SpringBootTest`, starting an actual server on a `RANDOM_PORT`
